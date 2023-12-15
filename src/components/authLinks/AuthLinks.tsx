6b0dc73edee44f67ac926157a0df798f50945e51
Alinur Shayakhmet <vashalinur@gmail.com>
2023-12-15 12:29:34 +0600

    blogapp project

--- src/components/authLinks/AuthLinks.tsx
+++ src/components/authLinks/AuthLinks.tsx
@@ -13,7 +13,7 @@ export default function AuthLinks() {
   
   return (
     <>
-      {status === "notAuthenticated" ? (
+      {status === "authenticated" ? (
         <Link href="/login">Login</Link>
       ) : (
         <>
